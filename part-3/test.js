4087c3ee81161a10318e699c6fbe7dbf97d5b896
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-01-27 10:36:24 +0800

    rpn video part-3

---
+++ part-3/test.js
@@ -0,0 +1,19 @@
+let angka1 = 10;
+let angka2 = 20;
+
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 * angka2);
+console.log(angka1 % angka2);
+
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 % angka2;
+console.log(penampungHasilOperatorAritmatika);
