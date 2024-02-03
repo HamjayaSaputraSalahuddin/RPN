0a9d058bcd44d63906b1175aa485084fb1fc7ffb
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-03 12:05:48 +0800

    fix console log error

--- Quiz-Before-Looping/quiz1.js
+++ Quiz-Before-Looping/quiz1.js
@@ -93,34 +93,34 @@ let exampleFirstWord4 = word4.substring(0, 3);
 
 let firstWordLength = exampleFirstWord4.length;
 
-console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
 
 //Second Word
 let exampleSecondWord4 = word4.substring(4, 14);
 
 let secondWordLength = exampleSecondWord4.length;
 
-console.log('Second Word: ' + exampleSecondWord + ', with length: ' + secondWordLength);
+console.log('Second Word: ' + exampleSecondWord4 + ', with length: ' + secondWordLength);
 
 //Third Word
 let exampleThirdWord4 = word4.substring(15, 17);
 
 let thirdWordLength = exampleThirdWord4.length;
 
-console.log('Third Word: ' + exampleThirdWord + ', with length: ' + thirdWordLength);
+console.log('Third Word: ' + exampleThirdWord4 + ', with length: ' + thirdWordLength);
 
 //Fourth Word
 let exampleFourthWord4 = word4.substring(18, 20);
 
 let fourthWordLength = exampleFourthWord4.length;
 
-console.log('Fourth Word: ' + exampleFourthWord + ', with length: ' + fourthWordLength);
+console.log('Fourth Word: ' + exampleFourthWord4 + ', with length: ' + fourthWordLength);
 
 //Fifth Word
 let exampleFifthWord4 = word4.substring(21, 25);
 
 let fifthWordLength = exampleFifthWord4.length;
 
-console.log('Fifth Word: ' + exampleFifthWord + ', with length: ' + fifthWordLength);
+console.log('Fifth Word: ' + exampleFifthWord4 + ', with length: ' + fifthWordLength);
 // ---------------------------------------
 console.log('==============================================');
