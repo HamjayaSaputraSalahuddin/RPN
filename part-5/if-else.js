0d5ef8f317c25cc38114d4229a678cc5d2aacc57
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-03 10:30:36 +0800

    if else practice

---
+++ part-5/if-else.js
@@ -0,0 +1,11 @@
+let nilai = 89;
+
+if (nilai == 100) {
+    console.log('A');
+} else if (nilai >= 90) {
+    console.log('A-');
+} else if (nilai >= 80) {
+    console.log('B+');
+} else {
+    console.log('C');
+}
