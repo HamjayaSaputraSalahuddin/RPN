339b69bf3cab1a9e5242ed5d0ba146d81e19fce2
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-04 14:54:47 +0800

    Looping with conditional statement

---
+++ looping-part-6/looping-part2.js
@@ -0,0 +1,12 @@
+// Looping with Conditional Statement
+
+let i = 0;
+
+while (i < 30) {
+    if (i % 2 === 0) {
+        console.log(`${i} adalah genap`);
+    } else {
+        console.log(`${i} adalah ganji`);
+    }
+    i++
+}
