9a41491b4d64749b3af95ab9e976668fd41969f5
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-04 14:30:38 +0800

    looping teory upload

---
+++ looping-part-6/for.js
@@ -0,0 +1,33 @@
+//While Looping
+
+// let i = 0;
+
+// while(i > 5) {
+//     console.log(i)
+//     i++
+// }
+
+//For Loop
+
+// for(i=0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// For String
+
+// let name = 'jelketss';
+// let result = '';
+
+// for (i = name.length -1; i >= 0; i-- ) {
+//     result += name[i];
+// }
+// console.log(result);
+
+// Nested Loop
+
+for (let i = 5; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+        console.log(j);
+    }
+    console.log('');
+}
