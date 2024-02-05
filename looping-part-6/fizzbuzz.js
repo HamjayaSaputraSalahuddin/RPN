a11302df6d227dbd021a71ef2a142d20aff315b0
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-05 08:45:37 +0800

    modify fizzbuzz logic value

--- looping-part-6/fizzbuzz.js
+++ looping-part-6/fizzbuzz.js
@@ -1,11 +1,11 @@
 //FizzBuzz Program Logic
 
 for (i = 0; i < 50; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 2 === 0 && i % 4 === 0) {
         console.log('fizzbuzz');
-    } else if (i % 3 === 0 && i % 5 !== 0 ) {
+    } else if (i % 2 === 0 && i % 4 !== 0 ) {
         console.log('fizz');
-    } else if (i % 5 === 0 && i % 3 !== 0 ) {
+    } else if (i % 4 === 0 && i % 2 !== 0 ) {
         console.log('buzz');
     } else {
         console.log(i);
