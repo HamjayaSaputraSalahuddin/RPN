fa4d8877c80b6bae8c9957687ae52ac649924756
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-05 10:29:41 +0800

    Quiz No 2 Upload

--- Quiz-Function/function-quiz.js
+++ Quiz-Function/function-quiz.js
@@ -11,3 +11,18 @@ function shoutOut() {
 }
 
 console.log(shoutOut());
+
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+console.log('==============================================');
+
+//bikin fungsinya disini
+let num1 = 1
+let num2 = 2
+function calculateMultiply(num1, num2) {
+    return num1 * num2;
+    
+} 
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
