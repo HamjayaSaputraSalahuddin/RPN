07dc94de920e36b7d0cc9e8eb406a2d2cfa393bd
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-05 10:17:34 +0800

    Quiz No 1 Upload

---
+++ Quiz-Function/function-quiz.js
@@ -0,0 +1,13 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+console.log('==============================================');
+
+console.log("Quiz No. 1");
+
+//bikin fungsinya disini
+function shoutOut() {
+    return 'Halo Function!'
+}
+
+console.log(shoutOut());
