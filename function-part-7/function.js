4e1144fd03ae93c7c545d8e9427a958f82f4fc6d
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-05 09:26:25 +0800

    create folder and file for function

--- function-part-7/function.js
+++ function-part-7/function.js
@@ -3,3 +3,20 @@ function helloWorld() {
 }
 
 console.log(helloWorld());
+
+function penjumlahan(angka1, angka2) {
+    return angka1 + angka2;
+}
+
+console.log(penjumlahan(12,12));
+console.log(penjumlahan(1,12));
+console.log(penjumlahan(2,12));
+
+function balikKata(kata) {
+    let result = "";
+    for(i = kata.length - 1; i >= 0; i--) {
+        result =+ kata[i];
+    }
+}
+
+console.log(balikKata('agus'));
