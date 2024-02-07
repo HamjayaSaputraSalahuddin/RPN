8bf432f266ebc01fa63256d4ec4176a2afd9ca89
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2024-02-07 09:06:22 +0800

    fix looping expression quiz no 2

--- Quiz-Week-1/week1-quiz.js
+++ Quiz-Week-1/week1-quiz.js
@@ -34,7 +34,7 @@ console.log("Quiz No. 2");
     function balikKata(kata) {
         // you can only write your code here!
         let result = "";
-        for (let i = kata.length - 1; i > 0; i--) {
+        for (let i = kata.length - 1; i >= 0; i--) {
             result += kata[i];
         }
         return result;
